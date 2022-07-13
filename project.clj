1549ed8e245c147486d6341153aab86cd7dc978a
Jaiane <jaiane.guimaraes@nubank.com.br>
2022-07-13 16:35:41 -0300

    :sparkles: Add render-static and uberjar

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.9.5"]
-                 [compojure "1.7.0"]]
+                 [compojure "1.7.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [yogthos/config "1.1.7"]
+                 [rum "0.12.9"]]
   :main ^:skip-aot friendwall.core
   :repl-options {:init-ns friendwall.core}
+  :target-path "target%s"
+  :aot [friendwall.core]
  )
--- src/friendwall/core.clj
+++ src/friendwall/core.clj
@@ -1,13 +1,26 @@
 (ns friendwall.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer :all]
-            [compojure.route :as route])
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [config.core :refer [env]]
+            [rum.core :refer [defc render-static-markup]])
   (:gen-class))
 
+(defc main-page []
+  [:div {:id "main-div"
+         :class "main-page-div"}
+   [:h1 "Hello Jaiane!"]])
+
+(defc friends-page []
+  [:id {:id "main-div"
+        :class "main-page-div"}
+   [:h1 "There are no friends yet, baby :("]])
+
 (defroutes app
-           (GET "/" [] "<h1>Hello World!</h1>")
-           (GET "/friends" [] "<h1>There are no friends yet :( </h1>"))
+           (GET "/" [] (render-static-markup (main-page)))
+           (GET "/friends" [] (render-static-markup (friends-page))))
 
 (defn -main
   [& _]
-  (run-jetty app {:port 80}))
+  (run-jetty (wrap-defaults app site-defaults) {:port (:port env)}))
